c432c9e85ee94fb1f2bf495e212cacb2412df6be
engelhaftes <divinegirl1980@gmail.com>
2026-03-05 11:04:44 +0300

    Create task037.js

---
+++ Циклы & Switch/task037.js
@@ -0,0 +1,17 @@
+function sieveOfEratosthenes(n) {
+    const isPrime = new Array(n + 1).fill(true);
+    isPrime[0] = isPrime[1] = false;
+    for (let i = 2; i * i <= n; i++) {
+        if (isPrime[i]) {
+            for (let j = i * i; j <= n; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+    const primes = [];
+    for (let i = 2; i <= n; i++) {
+        if (isPrime[i]) primes.push(i);
+    }
+    return primes;
+}
+console.log(sieveOfEratosthenes(20));
