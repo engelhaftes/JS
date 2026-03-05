92d802f75d0b51f9987321f7a606107539dd83cc
engelhaftes <divinegirl1980@gmail.com>
2026-03-05 11:05:08 +0300

    Create task038.js

---
+++ Циклы & Switch/task038.js
@@ -0,0 +1,8 @@
+function chunk(arr, size) {
+    const res = [];
+    for (let i = 0; i < arr.length; i += size) {
+        res.push(arr.slice(i, i + size));
+    }
+    return res;
+}
+console.log(chunk([1, 2, 3, 4, 5, 6], 2)); 
