e50398ea6c905bec66a22f28e74abbcee0b6af85
engelhaftes <divinegirl1980@gmail.com>
2026-03-05 11:03:26 +0300

    Create task033.js

---
+++ Циклы & Switch/task033.js
@@ -0,0 +1,16 @@
+function bubbleSort(arr) {
+    let n = arr.length;
+    let swapped;
+    do {
+        swapped = false;
+        for (let i = 1; i < n; i++) {
+            if (arr[i - 1] > arr[i]) {
+                [arr[i - 1], arr[i]] = [arr[i], arr[i - 1]];
+                swapped = true;
+            }
+        }
+        n--;
+    } while (swapped);
+    return arr;
+}
+console.log(bubbleSort([5, 3, 8, 4, 2]));
