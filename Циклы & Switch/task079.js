bb001e67d8a82d6f67faea35d1174628033666d4
engelhaftes <divinegirl1980@gmail.com>
2026-03-05 11:01:32 +0300

    Create task079.js

---
+++ Циклы & Switch/task079.js
@@ -0,0 +1,15 @@
+function varInCase(val) {
+    switch (val) {
+        case 1: {
+            let msg = 'One';
+            console.log(msg);
+            break;
+        }
+        case 2: {
+            let msg = 'Two';
+            console.log(msg);
+            break;
+        }
+    }
+}
+varInCase(1);
