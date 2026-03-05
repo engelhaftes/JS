47f113f338486b46d007f5a9b05be595a13f6acd
engelhaftes <divinegirl1980@gmail.com>
2026-03-05 11:08:42 +0300

    Create task048.js

---
+++ Циклы & Switch/task048.js
@@ -0,0 +1,12 @@
+function slidingWindowMax(arr, k) {
+    const res = [];
+    const deque = []; // Индексы
+    for (let i = 0; i < arr.length; i++) {
+        while (deque.length && arr[deque[deque.length - 1]] < arr[i]) deque.pop();
+        deque.push(i);
+        if (deque[0] === i - k) deque.shift();
+        if (i >= k - 1) res.push(arr[deque[0]]);
+    }
+    return res;
+}
+console.log(slidingWindowMax([1, 3, -1, -3, 5, 3, 6, 7], 3));
